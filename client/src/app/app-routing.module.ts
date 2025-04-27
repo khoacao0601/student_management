dc23c87582c1cc3f02570f66683c2a570f1bce34
Khoa Cao <khoacao0601@gmail.com>
2025-04-27 02:03:15 -0500

    Remove professor-tab component

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,15 +4,13 @@ import { AboutComponent } from './header/about/about.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { MainDetailsPageComponent } from './main-details-page/main-details-page.component';
-import { ProfessorTabComponent } from './header/professor-tab/professor-tab.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: HomePageComponent},
   {path: 'about', component: AboutComponent},
   {path: 'signUp', component: SignUpComponent},
-  {path: 'details', component: MainDetailsPageComponent},
-  {path: 'professor', component: ProfessorTabComponent}
+  {path: 'details', component: MainDetailsPageComponent}
 ];
 
 @NgModule({
