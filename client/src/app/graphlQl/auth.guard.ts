8704c064a82881f733708f762f31bcb088e61655
Khoa Cao <khoacao0601@gmail.com>
2025-05-14 23:24:41 -0500

    fix sonar

--- client/src/app/graphlQl/auth.guard.ts
+++ client/src/app/graphlQl/auth.guard.ts
@@ -4,7 +4,7 @@ import { AuthService } from './auth.service';
 
 @Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
-  constructor(private readonlyauth: AuthService, private readonly router: Router) {}
+  constructor(private readonly auth: AuthService, private readonly router: Router) {}
 
   canActivate(): boolean {
     if (this.auth.isLoggedIn()) return true;
